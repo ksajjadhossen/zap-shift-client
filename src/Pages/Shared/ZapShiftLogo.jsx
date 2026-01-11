22c1f9cefe06ce6ae8a8f17ae46bd485d473b8cc
ksajjadhossen <ksajjadhossen07@gmail.com>
2026-01-11 13:06:08 +0600

    root layout and navbar setup done

---
+++ src/Pages/Shared/ZapShiftLogo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../../assets/assets/logo.png";
+
+const ZapShiftLogo = () => {
+  return (
+    <div className=" flex">
+      <img src={logo} alt="logo" />
+      <h3 className="text-2xl">ZapShift</h3>
+    </div>
+  );
+};
+
+export default ZapShiftLogo;
